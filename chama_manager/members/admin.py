f70717477663647d72a25f3f516dff9d8bc0277c
David <davidmunyiri2@gmail.com>
2023-01-04 17:34:54 +0300

    created month and contribution model

--- chama_manager/members/admin.py
+++ chama_manager/members/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member,Contribution , Month
 
 # Register your models here.
 admin.site.register(Member)
+admin.site.register(Contribution)
+admin.site.register(Month)
--- chama_manager/members/models.py
+++ chama_manager/members/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from flask_sqlalchemy import Model
+from sqlalchemy import false
 
 
 
@@ -12,3 +14,32 @@ class Member(models.Model):
 
     def __str__(self):
         return self.first_name
+
+#Model of contribution
+class Contribution(models.Model):
+    """
+        Contribution detail of a member
+    """
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    contributed = models.BooleanField(default=false)
+
+
+#Model of Month
+class Month(models.Model):
+    months =[
+        ("JAN","January"),
+        ("FEB","February"),
+        ("MAR","March"),
+        ("APR","April"),
+        ("MAY","May"),
+        ("JUN","June"),
+        ("JUL","July"),
+        ("AUG","August"),
+        ("SEP","September"),
+        ("OCT","October"),
+        ("NOV","November"),
+        ("DEC","December"),    
+    ]
+    month = models.CharField( max_length=3 , choices=months)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
