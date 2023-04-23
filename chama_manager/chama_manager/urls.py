ca82ee812d3c264432649f745d4c8d23d6360105
David <davidmunyiri2@gmail.com>
2023-04-23 16:12:25 +0300

    logout working

--- chama_manager/chama_manager/urls.py
+++ chama_manager/chama_manager/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('members.urls')),
     path('home/', include('core.urls')),
-    path('',auth_views.LoginView.as_view(template_name='core/index.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
+    path('',auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='core/logout.html'),name='logout'),
 
 ]
