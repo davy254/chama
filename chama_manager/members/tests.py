8581fb9e746248fb8694fc9cd00a7969781d7839
David <davidmunyiri2@gmail.com>
2023-05-03 08:35:32 +0300

    Update tests.py

--- chama_manager/members/tests.py
+++ chama_manager/members/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from core.models import Contribution
+from .models import Member,Loan
+from .forms import MemberForm
 
 # Create your tests here.
+
+class ContributionModelTest(TestCase):
+    def setUp(self):
+        self.member = Member.objects.create(first_name="David", last_name="Batista")
+        self.contribution = Contribution.objects.create(member= self.member, 
+                                                        month = "May",
+                                                        amount= 200,
+                                                        contributed = True
+                                                         )
+        self.client = Client()
+
+
+
+    def test_create_member_view(self):
+        url = reverse('new-member')
+        response = self.client.post(url, {'first_name':self.member.first_name,
+                                            'last_name': self.member.last_name})
+        self.assertEqual(response.status_code, 200)
